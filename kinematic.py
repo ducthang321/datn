2316eca0dc243d74d1ed68507bee3bade5b7b4ae
ducthang321 <119120431+ducthang321@users.noreply.github.com>
2025-04-27 10:15:39 +0700

    Update kinematic.py

--- kinematic.py
+++ kinematic.py
@@ -1206,9 +1206,9 @@ class EEZYbotARM_Mk1(EEZYbotARM):
     LB3 = 35
 
     # Joint limits
-    q1_min = -30  # degrees
-    q1_max = 30
-    q2_min = 39
+    q1_min = -90  # degrees
+    q1_max = 90
+    q2_min = 3
     q2_max = 120
     # q3_min, q3_max are given by q3CalcLimits() function
 
@@ -1230,7 +1230,7 @@ class EEZYbotARM_Mk1(EEZYbotARM):
         q2 = kwargs.get('q2', self.q2)
 
         # calculate q3 min limits in degrees
-        q3_min = (-0.6755 * q2) - 70.768
+        q3_min = (-0.6755 * q2) - 140
         q3_max = (-0.7165 * q2) - 13.144
 
         return q3_min, q3_max
@@ -1269,7 +1269,8 @@ class EEZYbotARM_Mk1(EEZYbotARM):
         self.checkErrorJointLimits(q1=q1, q2=q2, q3=q3)
 
         # Calculate for q1
-        servoAngle_q1 = ((-2.0497)*q1) + 91.726  # from experimentation !
+	    #servoAngle_q1 = ((-2.0497)*q1) + 91.726
+        servoAngle_q1 = 90 + q1*(1.0497)  # from experimentation !
         servoAngle_q1 = round(servoAngle_q1, 2)
 
         # Calculate for q2
